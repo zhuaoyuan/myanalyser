0933c0e4f0decedbc5280a088c2080255ddac4a1
zhuaoyuan <zhuaoyuan@zhuaoyuandeMacBook-Pro.local>
2026-03-19 21:02:34 +0800

    v0319.07

--- tools/git/gitdiff.sh
+++ tools/git/gitdiff.sh
@@ -8,7 +8,7 @@ mkdir -p tmp
 COMMIT_ID=$(git rev-parse --short HEAD)
 
 # 获取当前时间戳，格式为：YYYYMMDD_HHMM (例如：20260311_0945)
-TIMESTAMP=$(date +"%Y%m%d_%H%M")
+TIMESTAMP=$(date +"%Y%m%d%H%M")
 
 # 定义文件名
 FILENAME="gitdiff_tmp/temp_diff_${TIMESTAMP}_${COMMIT_ID}.diff"
